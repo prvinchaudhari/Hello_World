4fcf5dc1d56cb00b1024299b1e33735c6b9c3dfd
Pravin Chaudhari <[prvinchaudhari@gmail.com]>
2026-01-28 10:34:12 +0530

    Adding new java class file

---
+++ Addition.java
@@ -0,0 +1,7 @@
+public class Addition{
+    public static void main(String[] args){
+        int firstNumber=20;
+        int secondNumber=30;
+        System.out.println("Addition Is :- "+(firstNumber+secondNumber));
+    }
+}
